8e6c952d45d191d70cfe0c789aeda45e6b050105
root-11 <bjorn.h.madsen@gmail.com>
2019-02-23 20:12:41 +0000

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,267 @@
+from functools import lru_cache
+from itertools import combinations
+from collections import defaultdict
+from heapq import heappop, heappush
+
+
+__all__ = ['Graph']
+
+
+class Graph(object):
+    """
+    Graph is the base graph that all methods use.
+
+    """
+    def __init__(self, nodes=None, links=None):
+        if nodes is None:
+            nodes = {}
+        self.nodes = nodes
+        if links is None:
+            links = {}
+        self.links = links
+        self._max_length = 0
+        self._edges_cache = None
+
+    def __getitem__(self, item):
+        return self.links.__getitem__(item)
+
+    def edges(self):
+        """
+        :return: list of edges (n1, n2, distance)
+        """
+        if self._edges_cache:
+            return self._edges_cache[:]
+
+        L = []
+        for n1 in self.links:
+            for n2 in self.links[n1]:
+                L.append((n1, n2, self.links[n1][n2]))
+        return L
+
+    def add_node(self, node_id):
+        self.nodes[node_id] = 1
+
+    def add_link(self, node1, node2, distance=1, bidirectional=False):
+        """
+        :param node1: hashable node
+        :param node2: hashable node
+        :param distance: numeric value.
+        :param bidirectional: boolean.
+        """
+        self._edges_cache = None  #
+
+        assert isinstance(distance, (float, int))
+        self.add_node(node1)
+        self.add_node(node2)
+        if node1 not in self.links:
+            self.links[node1] = {}
+        if node2 not in self.links:
+            self.links[node2] = {}
+        self.links[node1][node2] = distance
+        if distance > self._max_length:
+            self._max_length = distance
+        if bidirectional:
+            self.links[node2][node1] = distance
+
+    def update_from_dict(self, dictionary):
+        """
+        Creates graph from dictionary
+        :param dictionary:
+
+        d = {1: {2: 10, 3: 5},
+             2: {4: 1, 3: 2},
+             3: {2: 3, 4: 9, 5: 2},
+             4: {5: 4},
+             5: {1: 7, 4: 6}}
+        G = from_dict(d)
+
+        :return: class Graph.
+        """
+        for n1 in dictionary:
+            for n2 in dictionary[n1]:
+                self.add_link(n1, n2, dictionary[n1][n2])
+
+    # methods:
+    def shortest_path(self, start, end):
+        return shortest_path(graph=self, start=start, end=end)
+
+    def distance_from_path(self, path):
+        return distance(graph=self, path=path)
+
+    def solve_tsp(self):
+        tsp = TSP(nodes=self.nodes, links=self.links)
+        path = tsp.solve()
+        length = tsp.tsp_tour_length(path)
+        return length, path
+
+    def subgraph_from_nodes(self, nodes):
+        return subgraph(graph=self, nodes=nodes)
+
+    def same_path(self, p1, p2):
+        return same(p1, p2)
+
+
+def shortest_path(graph, start, end):
+    """
+    :param graph: class Graph
+    :param start: start node
+    :param end: end node
+    :return: distance, path (as list),
+             returns float('inf'), [] if no path exists.
+    """
+    assert isinstance(graph, Graph)
+
+    g = defaultdict(list)
+    for n1, n2, dist in graph.edges():
+        g[n1].append((dist, n2))
+
+    q, visited, mins = [(0, start, ())], set(), {start: 0}
+    while q:
+        (cost, v1, path) = heappop(q)
+        if v1 not in visited:
+            visited.add(v1)
+            path = (v1, path)
+            if v1 == end:
+                L = []
+                while path:
+                    v, path = path[0], path[1]
+                    L.append(v)
+                L.reverse()
+                return cost, L
+
+            for dist, v2 in g.get(v1, ()):
+                if v2 in visited:
+                    continue
+                prev = mins.get(v2, None)
+                next = cost + dist
+                if prev is None or next < prev:
+                    mins[v2] = next
+                    heappush(q, (next, v2, path))
+
+    return float("inf"), []
+
+
+def distance(graph, path):
+    assert isinstance(graph, Graph)
+    assert isinstance(path, list)
+    d = 0
+    for idx in range(len(path)-1):
+        n1, n2 = path[idx], path[idx+1]
+        d += graph.links[n1][n2]
+    return d
+
+
+def subgraph(graph, nodes):
+    assert isinstance(graph, Graph)
+    assert isinstance(nodes, list)
+    assert all(n1 in graph.nodes for n1 in nodes)
+    G = Graph()
+    for n1 in nodes:
+        G.add_node(n1)
+        for n2 in graph[n1]:
+            G.add_link(n1, n2, graph[n1][n2])
+    return G
+
+
+def same(path1, path2):
+    """ Compares two paths to verify whether they're the same.
+    :param path1: list of nodes.
+    :param path2: list of nodes.
+    :return: boolean.
+    """
+    start1 = path2.index(path1[0])
+    checks = [
+        path1[:len(path1) - start1] == path2[start1:],
+        path1[len(path1) - start1:] == path2[:start1]
+    ]
+    if all(checks):
+        return True
+    return False
+
+
+class TSP(Graph):
+    def __init__(self, nodes=None, links=None):
+        super().__init__(nodes, links)
+        self.cache = {}
+
+    def shortest_links_first(self):
+        """ returns a list of (distance, node1, node2) with shortest on top."""
+        c = combinations(self.nodes, 2)
+        distances = [(self.links[a][b], a, b) for a, b in c]
+        distances.sort()
+        return distances
+
+    def reverse_segment_if_improvement(self, tour, i, j):
+        """If reversing tour[i:j] would make the tour shorter, then do it."""
+        # Given tour [...A,B...C,D...], consider reversing B...C to get [...A,C...B,D...]
+        A, B, C, D = tour[i - 1], tour[i], tour[j - 1], tour[j % len(tour)]
+        # Are old links (AB + CD) longer than new ones (AC + BD)? If so, reverse segment.
+        if self.links[A][B] + self.links[C][D] > self.links[A][C] + self.links[B][D]:
+            tour[i:j] = reversed(tour[i:j])
+            return True
+
+    def improve_tour(self, tour):
+        if not tour:
+            raise ValueError("No tour to improve?")
+
+        while True:
+            improvements = {self.reverse_segment_if_improvement(tour, i, j)
+                            for (i, j) in self.subsegments(len(tour))}
+            if improvements == {None} or len(improvements) == 0:
+                return tour
+
+    @staticmethod
+    @lru_cache()
+    def subsegments(N):
+        """ Return (i, j) pairs denoting tour[i:j] subsegments of a tour of length N."""
+        return [(i, i + length) for length in reversed(range(2, N))
+                for i in reversed(range(N - length + 1))]
+
+    @staticmethod
+    def join_endpoints(endpoints, A, B):
+        "Join segments [...,A] + [B,...] into one segment. Maintain `endpoints`."
+        Aseg, Bseg = endpoints[A], endpoints[B]
+        if Aseg[-1] is not A: Aseg.reverse()
+        if Bseg[0] is not B: Bseg.reverse()
+        Aseg += Bseg
+        del endpoints[A]
+        del endpoints[B]
+        endpoints[Aseg[0]] = endpoints[Aseg[-1]] = Aseg
+        return Aseg
+
+    def tsp_tour_length(self, tour):
+        """ The TSP tour length with return to the starting point."""
+        return sum(self.links[tour[i - 1]][tour[i]] for i in range(len(tour)))
+
+    def solve(self):
+        """ Attempts to solve the TSP
+
+        Runtime approximation: seconds = 10**(-5) * (points)**2.31
+        Solution quality: Range 98.1% - 100% optimal.
+
+        """
+        # The core TSP solver:
+        # 1. create a path using greedy algorithm (picks nearest peer)
+        new_segment = []
+        endpoints = {n: [n] for n in self.nodes}
+        L = self.shortest_links_first()
+        for _, a, b in L:
+            if a in endpoints and b in endpoints and endpoints[a] != endpoints[b]:
+                new_segment = self.join_endpoints(endpoints, a, b)
+                if len(new_segment) == len(self.nodes):
+                    break  # return new_segment
+        assert len(new_segment) == len(self.nodes)
+        first_tour = new_segment[:]
+        first_path_length = self.tsp_tour_length(first_tour)
+
+        # 2. run improvement on the created path.
+        improved_tour = self.improve_tour(new_segment)
+        assert set(self.nodes) == set(improved_tour)
+
+        second_path_length = self.tsp_tour_length(improved_tour)
+
+        assert first_path_length >= second_path_length, "first path was better than improved tour?! {} {}".format(
+            first_path_length, second_path_length
+        )
+
+        return improved_tour
---
+++ graph_tests.py
@@ -0,0 +1,201 @@
+import time
+import random
+from itertools import combinations
+from graphs import Graph
+
+
+def test01():
+    """
+    Asserts that the shortest_path is correct
+    """
+    d = {1: {2: 10, 3: 5},
+         2: {4: 1, 3: 2},
+         3: {2: 3, 4: 9, 5: 2},
+         4: {5: 4},
+         5: {1: 7, 4: 6}}
+    G = Graph()
+    G.update_from_dict(d)
+
+    dist, path = G.shortest_path(1, 4)
+    assert [1, 3, 2, 4] == path, path
+    assert 9 == dist, dist
+
+
+def test02():
+    """
+    Assert that the dict loader works.
+    """
+    d = {1: {2: 10, 3: 5},
+         2: {4: 1, 3: 2},
+         3: {2: 3, 4: 9, 5: 2},
+         4: {5: 4},
+         5: {1: 7, 4: 6}}
+    G = Graph()
+    G.update_from_dict(d)
+    assert d[3] == G[3]
+    assert d[3][4] == G[3][4]
+
+
+def test_graph_data():
+    """triggers error in priorityDictionary
+    g and h are two dictionaries with exactly same structure, but some distances vary
+    g triggers an error and h doesn't
+    """
+    g = {1: {2: 1, 3: 9, 4: 4, 5: 13, 6: 20},
+         2: {1: 7, 3: 7, 4: 2, 5: 11, 6: 18},
+         3: {8: 20, 4: 4, 5: 4, 6: 16, 7: 16},
+         4: {8: 15, 3: 4, 5: 9, 6: 11, 7: 21},
+         5: {8: 11, 6: 2, 7: 17},
+         6: {8: 9, 7: 5},
+         7: {8: 3},
+         8: {7: 5}}
+    h = {1: {2: 1, 3: 9, 4: 4, 5: 11, 6: 17},
+         2: {1: 7, 3: 7, 4: 2, 5: 9, 6: 15},
+         3: {8: 17, 4: 4, 5: 4, 6: 14, 7: 13},
+         4: {8: 12, 3: 4, 5: 9, 6: 9, 7: 18},
+         5: {8: 9, 6: 2, 7: 15},
+         6: {8: 9, 7: 5},
+         7: {8: 3},
+         8: {7: 5}}
+
+    G = Graph()
+    G.update_from_dict(h)
+    dist, path = G.shortest_path(1, 8)
+    assert path == [1, 2, 4, 8], path
+    G = Graph()
+    G.update_from_dict(g)
+    dist, path = G.shortest_path(1, 8)
+    assert path == [1, 2, 4, 8], path
+
+
+def test_same():
+    p1 = [1, 2, 3, 4, 5]
+    p2 = [3, 4, 5, 1, 2]
+    g = Graph()
+    assert g.same_path(p1, p2)
+
+
+def test_tsp():
+    """
+    Assures that the optimization method works.
+
+    The greedy algorithm of the TSP solvers first method
+    will build this graph:
+
+    (0)---(1)---(2)---(3)---(4)
+       \                     /
+        ----------------------------------------
+                          /                     \
+                        (5)---(6)---(7)---(8)---(9)
+
+    The method 'improve tour' will then reverse segments
+    if they improve the overall tour length. This leads to
+    this result:
+
+    (0)---(1)---(2)---(3)---(4)--------------\
+       \                                      \
+        \                                      \
+          \                                     \
+            \-----------(5)---(6)---(7)---(8)---(9)
+
+    which is a fraction shorter.
+    """
+    g = Graph()
+    xys = [
+        (0, 0),
+        (0, 1),
+        (0, 2),
+        (0, 3),
+        (0, 4),
+        (1, 3),
+        (1, 4),
+        (1, 5),
+        (1, 6),
+        (1, 7)
+    ]
+
+    def distance(a, b):
+        dx = abs(a[0] - b[0])
+        dy = abs(a[1] - b[1])
+        return (dx ** 2 + dy ** 2) ** (1 / 2)
+
+    # The graph must be fully connected for the TSP to work:
+    for a, b in combinations(range(len(xys)), 2):
+        d = distance(xys[a], xys[b])
+        g.add_link(a, b, distance=d)
+        g.add_link(b, a, distance=d)
+
+    dist, path = g.solve_tsp()
+    expected_tour = [0, 1, 2, 3, 4, 9, 8, 7, 6, 5]
+    expected_length = 14.32455532033676
+    assert dist == expected_length, (dist, expected_length)
+    assert g.same_path(path, expected_tour)
+
+
+def test_tsp_perfect_problem():
+    """
+    The tour is:
+
+    (0,5)--(1,5)--(2,5)--(3,5)--(4,5)--(5,5)
+      |                                  |
+    (0,4)                              (5,4)
+      |                                  |
+    (0,3)                              (5,3)
+      |                                  |
+    (0,2)                              (5,2)
+      |                                  |
+    (0,1)--(1,1)--(2,1)--(3,1)--(4,1)--(5,1)
+
+    And it must be 18 long.
+    """
+    g = Graph()
+    xys = [
+        (0, 1), (0, 2), (0, 3), (0, 4), (0, 5),
+        (1, 5), (2, 5), (3, 5), (4, 5), (5, 5),
+        (5, 4), (5, 3), (5, 2),
+        (5, 1), (4, 1), (3, 1), (2, 1), (1, 1),
+    ]
+
+    def distance(a, b):
+        dx = abs(a[0] - b[0])
+        dy = abs(a[1] - b[1])
+        return (dx ** 2 + dy ** 2) ** (1 / 2)
+
+    # The graph must be fully connected for the TSP to work:
+    for a, b in combinations(range(len(xys)), 2):
+        d = distance(xys[a], xys[b])
+        g.add_link(a, b, distance=d)
+        g.add_link(b, a, distance=d)
+
+    dist, path = g.solve_tsp()
+    expected_tour = [i for i in range(len(xys))]
+    expected_length = len(xys)
+    assert dist == expected_length, (dist, expected_length)
+    g.same_path(path, expected_tour)
+
+
+def test_tsp_larger_problem():
+    random.seed(44)
+    points = 200
+
+    xys = set()
+    while len(xys) != points:
+        xys.add((random.randint(0, 600), random.randint(0, 800)))
+    xys = [n for n in xys]
+
+    g = Graph()
+    for a, b in combinations(xys, 2):
+        dx = abs(a[0] - b[0])
+        dy = abs(a[1] - b[1])
+        d = (dx ** 2 + dy ** 2) ** (1 / 2)
+        g.add_link(a, b, distance=d)
+        g.add_link(b, a, distance=d)
+
+    start = time.clock()
+    dist, path = g.solve_tsp()
+    end = time.clock()
+    print("Running tsp on {} points, took {:.3f} seconds".format(points, end - start))
+    assert len(path) == points
+
+
+
