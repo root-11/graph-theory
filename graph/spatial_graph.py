8b032477e79d54f683e6a02bada13e0a5d1f206b
root-11 <bjorn.h.madsen@gmail.com>
2020-01-27 12:02:31 +0000

    Added new features on Graph3D and added tests to increase code coverage to 100%.

--- graph/spatial_graph.py
+++ graph/spatial_graph.py
@@ -1,5 +1,6 @@
 from itertools import count
 from graph import BasicGraph
+from graph.search import shortest_path
 
 
 class Graph3D(BasicGraph):
@@ -34,7 +35,7 @@ class Graph3D(BasicGraph):
         a = abs(x2-x1)
         b = abs(y2-y1)
         c = abs(z2-z1)
-        return (a * a + b + b + c + c) ** (1 / 2)
+        return (a * a + b * b + c * c) ** (1 / 2)
 
     def add_edge(self, n1, n2, value=None, bidirectional=False):
         self._check_tuples(n1)
@@ -65,40 +66,85 @@ class Graph3D(BasicGraph):
         d = [(self.distance(n1=node_id, n2=n), n) for n in self.nodes() if n != node_id]
         d.sort()
         if d:
-            return [b for a, b in d][1:n+1]
+            return [b for a, b in d][:n]
         return None
 
-    def plot(self, nodes=True, edges=True):
-        """ plots nodes and links using matplotlib3"""
+    def shortest_path(self, start, end):
+        return shortest_path(self, start, end)
+
+    def plot(self, nodes=True, edges=True, rotation='xyz', maintain_aspect_ratio=False):
+        """ plots nodes and links using matplotlib3
+        :param nodes: bool: plots nodes
+        :param edges: bool: plots edges
+        :param rotation: str: set view point as one of [xyz,xzy,yxz,yzx,zxy,zyx]
+        :param maintain_aspect_ratio: bool: rescales the chart to maintain aspect ratio.
+        :return: None. Plots figure.
+        """
         from mpl_toolkits.mplot3d import Axes3D  # import required by matplotlib.
         import matplotlib.pyplot as plt
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
+        if not len(rotation) == 3:
+            raise ValueError(f"expected viewpoint as 'xyz' but got: {rotation}")
+        for c in 'xyz':
+            if c not in rotation:
+                raise ValueError(f"rotation was missing {c}.")
+        x, y, z = rotation
 
         # Data for a three-dimensional line
         if edges:
-
             for edge in self.edges():
                 n1, n2, v = edge
-                xe, ye, ze = [], [], []
-                xe.extend([n1[0], n2[0]])
-                ye.extend([n1[1], n2[1]])
-                ze.extend([n1[2], n2[2]])
-                ax.plot3D(xe, ye, ze, 'gray')
+                xyz = dict()
+                xyz[x] = [n1[0], n2[0]]
+                xyz[y] = [n1[1], n2[1]]
+                xyz[z] = [n1[2], n2[2]]
+                ax.plot3D(xyz['x'], xyz['y'], xyz['z'], 'gray')
 
         # Data for three-dimensional scattered points
         if nodes:
-            xn, yn, zn, ix = [], [], [], []
+            xyz = {x: [], y: [], z: []}
+            ix = []
             for idx, node in enumerate(self.nodes()):
-                x, y, z = node
-                xn.append(x)
-                yn.append(y)
-                zn.append(z)
+                vx, vy, vz = node  # value of ...
+                xyz[x].append(vx)
+                xyz[y].append(vy)
+                xyz[z].append(vz)
                 ix.append(idx)
-            ax.scatter3D(xn, yn, zn, c=ix, cmap='Greens')
+            ax.scatter3D(xyz['x'], xyz['y'], xyz['z'], c=ix, cmap='Greens')
 
-        ax.set_xlabel('X Label')
-        ax.set_ylabel('Y Label')
-        ax.set_zlabel('Z Label')
+        if (nodes or edges) and maintain_aspect_ratio:
+            nodes = [n for n in self.nodes()]
+            xyz_dir = {'x': 0, 'y': 1, 'z': 2}
+
+            xdim = xyz_dir[x]  # select the x dimension in the projection.
+            # as the rotation will change the xdimension index.
+            xs = [n[xdim] for n in nodes]  #  use the xdim index to obtain the values.
+            xmin, xmax = min(xs), max(xs)
+            dx = (xmax + xmin) / 2  # determine the midpoint for the dimension.
+
+            ydim = xyz_dir[y]
+            ys = [n[ydim] for n in nodes]
+            ymin, ymax = min(ys), max(ys)
+            dy = (ymax + ymin) / 2
+
+            zdim = xyz_dir[z]
+            zs = [n[zdim] for n in nodes]
+            zmin, zmax = min(zs), max(zs)
+            dz = (zmax + zmin) / 2
+
+            # calculate the radius for the aspect ratio.
+            max_dim = max([xmax - xmin, ymax - ymin, zmax - zmin]) / 2
+
+            xa, xb = dx - max_dim, dx + max_dim  # lower, uppper
+            ax.set_xlim(xa, xb)  # apply the lower and upper to the axis.
+            ya, yb = dy - max_dim, dy + max_dim
+            ax.set_ylim(ya, yb)
+            za, zb = dz - max_dim, dz + max_dim
+            ax.set_zlim(za, zb)
+
+        ax.set_xlabel(f'{x} Label')
+        ax.set_ylabel(f'{y} Label')
+        ax.set_zlabel(f'{z} Label')
         plt.show()
 
--- tests/test_spatial_graph.py
+++ tests/test_spatial_graph.py
@@ -1,27 +1,125 @@
-from math import sin, cos
+from math import sin, cos, isclose
 from graph.spatial_graph import Graph3D
 
 
-def test_basics():
-    xyz = [(sin(i / 150), cos(i / 150), i / 150) for i in range(0, 1500, 20)]
+def spiral_graph():
+    xyz = [(sin(i / 150), cos(i / 150), i / 150) for i in range(0, 1500, 30)]
     g = Graph3D()
     for t in xyz:
         g.add_node(t)
 
-    for n1 in g.nodes():
-        nn = g.n_nearest_neighbours(n1)[0]
-        distance = g.distance(n1, nn)
-        g.add_edge(n1, nn, distance)
+    xyz.sort(key=lambda x: x[2])  # sorted by z axis which is linear.
 
-    g.plot()
+    n1 = xyz[0]
+    for n2 in xyz[1:]:
+        distance = g.distance(n1, n2)
+        g.add_edge(n1, n2, distance)
+        n1 = n2
+    return g
+
+
+def fishbone_graph():
+    g = Graph3D()
+    j1 = None
+    for j2 in range(5):  # z axis.
+        g.add_node((0, 0, j2))
+        if j1 is None:
+            pass
+        else:
+            g.add_edge((0, 0, j1), (0, 0, j2), value=1, bidirectional=True)
+
+        s1, l1, r1 = None, None, None  # (s)pine, (l)eft side, (r)ight side
+        for i in range(10):  # step along the x axis.
+            s2, l2, r2 = (i, 0, j2), (i, 1, j2), (i, -1, j2)
+            g.add_node(s2)  # spine
+            g.add_node(l2)  # left rib
+            g.add_node(r2)  # right rib
+            if s1 is None:
+                pass
+            else:
+                g.add_edge(s1, s2, 1)
+            g.add_edge(s2, l2, 1, bidirectional=True)
+            g.add_edge(s2, r2, 1, bidirectional=True)
+
+            s1, l1, r1 = s2, l2, r2
+        j1 = j2
+
+    g.add_node((-1, 0, 2))  # entry point
+    g.add_edge((-1, 0, 2), (0, 0, 2), 1, bidirectional=True)
+    g.add_node((-1, 0, 1))  # exit point
+    g.add_edge((-1, 0, 1), (0, 0, 1), 1, bidirectional=True)
+    return g
+
+
+def test_basics():
+    g = Graph3D()
+    a, b, c = (0, 0, 0), (1, 1, 1), (2, 2, 2)
+    g.add_node(a)
+    g.add_node(b)
+    g.add_node(c)
+    assert g.n_nearest_neighbours(a)[0] == b
+    assert g.n_nearest_neighbours(c)[0] == b
 
     L = g.to_list()
     g2 = Graph3D(from_list=L)
+    assert g2.nodes() == g.nodes()
+    assert g2.edges() == g.edges()
 
     d = g.to_dict()
     g3 = Graph3D(from_dict=d)
+    assert g3.nodes() == g.nodes()
+    assert g3.edges() == g.edges()
 
     g4 = g.__copy__()
+    assert g4.edges() == g.edges()
+
+
+def test_path_finding():
+    xyz = [(sin(i / 150), cos(i / 150), i / 150) for i in range(0, 1500, 30)]
+    g = Graph3D()
+    for t in xyz:
+        g.add_node(t)
+
+    xyz.sort(key=lambda x: x[2])
+
+    total_distance = 0.0
+    n1 = xyz[0]
+    for n2 in xyz[1:]:
+        distance = g.distance(n1, n2)
+        total_distance += distance
+        g.add_edge(n1, n2, distance)
+        n1 = n2
+
+    d, p = g.shortest_path(xyz[0], xyz[-1])
+    assert isclose(d, 13.847754085278877), d
+    assert p == xyz, [(idx, a, b) for idx, (a, b) in enumerate(zip(p, xyz)) if a != b]
+
+
+def test_plotting():
+    g = spiral_graph()
+    g.plot()
+
+    g = fishbone_graph()
+    g.plot()
+    g.plot(rotation='yxz')
+    g.plot(maintain_aspect_ratio=True)
+
+    try:
+        g.plot(rotation='x')
+    except ValueError:
+        pass
+    try:
+        g.plot(rotation='abc')
+    except ValueError:
+        pass
+
+
+def test_shortest_path():
+    g = fishbone_graph()
+    entry_point = (-1, 0, 2)
+    exit_point = (-1, 0, 1)
+    d, p = g.shortest_path(entry_point, exit_point)
+    assert d == 3
 
 
 def test_no_nearest_neighbour():
