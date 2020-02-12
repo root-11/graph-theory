c18330d5bc29c05c44c4631e9036df42941466b3
root-11 <bjorn.h.madsen@gmail.com>
2020-02-12 15:36:44 +0000

    Added profiler to check number of calls.

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,17 +4,22 @@ import pstats
 
 
 def profileit(func):
-    """ decorator for profiling function. """
+    """ decorator for profiling function.
+    usage:
+    >>> def this(var1, var2):
+            # do something
+
+    >>> new_this = count_calls(this)
+    >>> calls, cprofile_text = new_this(var1=1,var2=2)
+    """
     def wrapper(*args, **kwargs):
-        datafn = func.__name__ + ".txt"  # Name the data file sensibly
         prof = cProfile.Profile()
         retval = prof.runcall(func, *args, **kwargs)
         s = io.StringIO()
         sortby = 'cumulative'
         ps = pstats.Stats(prof, stream=s).sort_stats(sortby)
         ps.print_stats()
-        with open(datafn, 'w') as perf_file:
-            perf_file.write(s.getvalue())
-        return retval
-
+        text = s.getvalue()
+        calls = int(text.split('\n')[0].lstrip().split(" ")[0])
+        return calls, text
     return wrapper
