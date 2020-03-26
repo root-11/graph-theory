e79959ef0ed6e53d313502659bda10b4a25b5c1c
root-11 <bjorn.h.madsen@gmail.com>
2020-03-26 19:02:09 +0000

    Take 2 on the solver for the scheduling problem.

--- graph/scheduling_problem.py
+++ graph/scheduling_problem.py
@@ -9,30 +9,55 @@ class SchedulingError(ValueError):
 class Task(object):
     _ids = count()
 
-    def __init__(self, name):
-        self.id = next(Task._ids)
-        self.name = name
-        # The values below are set by the Task.
-        self.scheduled = False
-        self.earliest_start = None
-        self.latest_finish = None
+    def __init__(self, requires, earliest_start=None, latest_finish=None):
+        """
+        :param requires: multiple types:
+            dicts are taken as given.
+            sets, lists, tuples are transformed to dicts with key: count(key)
+            strings are handled as {str: 1}
+        :param earliest_start: sortable value
+        :param latest_finish: sortable value
+        """
+        if isinstance(requires, (set, tuple, list)):
+            self.requires = {k: requires.count(k) for k in set(requires)}
+        elif isinstance(requires, str):
+            self.requires = {requires: 1}
+        elif isinstance(requires, dict):
+            self.requires = requires
+        else:
+            raise TypeError(f"requires expected dict, tuple, list, set or str, but got {type(requires)}")
+
+        self.earliest_start = earliest_start
+        self.latest_finish = latest_finish
         # The values below are set by the Resource
         self.earliest_finish = None
         self.latest_start = None
         self.duration = None
         self.cost = None
 
+        self.scheduled_start = None
+        self.scheduled_finish = None
+
+        self.id = next(self._ids)
+
+    def __eq__(self, other):
+        """ returns True if process.output.keys() == task.requires.keys()"""
+        if isinstance(other, Process):
+            return self.requires.keys() == other.outputs.keys()
+
+        raise NotImplementedError  # ... if we haven't returned above ...
+
+    def __bool__(self):
+        return self.scheduled_start is not None and self.scheduled_finish is not None
+
     def __str__(self):
-        return f"Task {self.name}"
+        return f"{self.__class__.__name__}({self.id}) {self.requires}"
 
     def __hash__(self):
         return self.id
 
-    def __bool__(self):
-        return self.scheduled
-
     def copy(self):
-        return Task(self.name)
+        return Task(self.requires)
 
 
 class Resource(object):
@@ -42,10 +67,12 @@ class Resource(object):
     _ids = count()
 
     def __init__(self):
-        self.id = next(Resource._ids)
-        self._rdn = None
+        self.id = next(self._ids)
+        self.mq = []
         self.tasks = []
+        self._task_ids = set()
         self.processes = []
+        self._rdn = None
 
     def __str__(self):
         return f"{self.__class__.__name__}({self.id})"
@@ -60,13 +87,28 @@ class Resource(object):
     @rdn.setter
     def rdn(self, value):
         if not isinstance(value, ResourceDemandNetwork):
-            raise ValueError("Node.rdn is a reserved property.")
+            err = f"{self.__class__.__name__}.rdn can only be set by the {ResourceDemandNetwork.__name__}"
+            raise ValueError(err)
+        if self._rdn is not None:
+            raise ValueError(f"{self}.rdn has already been set.")
         self._rdn = value
 
     def add_task(self, task):
-        t = Task(task)
-        assert isinstance(t, Task)
+        assert isinstance(task, Task)
+        if task.id in self._task_ids:
+            raise ValueError(f"{task} is already registered with {self}")
+        else:
+            self._task_ids.add(task.id)
         self.tasks.append(task)
+        self.mq.append(task.id)
+
+    def remove_task(self, task):
+        assert isinstance(task, Task)
+        self._task_ids.discard(task.id)
+        if task in self.tasks:
+            self.tasks.remove(task)
+        if task in self.mq:
+            self.mq.append(task.id)
 
     def add_process(self, proc):
         assert isinstance(proc, Process)
@@ -75,19 +117,35 @@ class Resource(object):
     def can_support(self, task):
         for process in self.processes:
             assert isinstance(process, Process)
-            if task.name in process.outputs:
+            if process == task:
                 return True
         return False
 
-    def suppliers(self):
-        if not isinstance(self._rdn, ResourceDemandNetwork):
-            raise AttributeError("Add Node to ResourceDemandNetwork first.")
-        rdn = self._rdn.network
-        assert isinstance(rdn, Graph)
-        return [rdn.node(nid) for nid in rdn.nodes(from_node=self.id)]
-
     def schedule(self):
-        """ find suppliers from self.rdn and assign tasks """
+        """ calculates the schedule. Stores the schedule in self.tasks """
+        if not isinstance(self._rdn, ResourceDemandNetwork):
+            raise AttributeError(f"Use ResourceDemandNetwork.add_resource({self}) first.")
+
+        # discover tasks
+        # for each task:
+        #   find supply requirements.
+        #   find suppliers
+        #   for each supplier:
+        #       create task
+        #       add task to supplier
+
+        # if all supply tasks are scheduled for delivery:
+        # for each task:
+        #   put own tasks in sequence
+
+        # sort to schedule.
+        # detect idletime, lateness, ...
+        # remove any duplicate tasks (and ping suppliers to update delivery times).
+
+        # notify customer of change in delivery times.
+
+        suppliers = [r_id for r_id in self._rdn.network.nodes(from_node=self.id)]
+
         undone = [t for t in self.tasks if not t]
         all_suppliers = set()
         for task in undone:
@@ -97,7 +155,7 @@ class Resource(object):
                 assert isinstance(supplier, Resource)
                 if supplier.can_support(task):
                     supplier_short_list.add(supplier)
-                    supplier.add_task(task.copy())
+                    supplier.add_task(task.copy())  # <--- this isn't right.
 
             if not supplier_short_list:
                 raise SchedulingError(f"No suppliers for {task} with {self}")
@@ -109,16 +167,44 @@ class Resource(object):
 
 
 class Process(object):
-    def __init__(self,
-                 inputs=None, outputs=None,
-                 setup_time=0, run_time=0, shutdown_time=0, change_over_time=0,
-                 cost=0):
-        if inputs is not None:
-            assert isinstance(inputs, list)
-        self.inputs = inputs
-        if outputs is not None:
-            assert isinstance(outputs, list)
-        self.outputs = outputs
+    def __init__(self, inputs=None, outputs=None,
+                 setup_time=0, run_time=0, shutdown_time=0, change_over_time=0, cost=0):
+        """
+        :param inputs: multiple types:
+            dicts are taken as given.
+            sets, lists, tuples are transformed to dicts with key: count(key)
+            strings are handled as {str: 1}
+        :param outputs: multiple types:
+            dicts are taken as given.
+            sets, lists, tuples are transformed to dicts with key: count(key)
+            strings are handled as {str: 1}
+        :param setup_time: any sortable value
+        :param run_time: any sortable value
+        :param shutdown_time: any sortable value
+        :param change_over_time: any sortable value
+        :param cost: any sortable value
+        """
+        if inputs is None:
+            self.inputs = {}
+        elif isinstance(inputs, (set, tuple, list)):
+            self.inputs = {k: inputs.count(k) for k in set(inputs)}
+        elif isinstance(inputs, str):
+            self.inputs = {inputs: 1}
+        elif isinstance(inputs, dict):
+            self.inputs = inputs
+        else:
+            raise TypeError(f"inputs expected set, tuple, list or dict, but got {type(inputs)}")
+
+        if outputs is None:
+            self.outputs = {}
+        elif isinstance(outputs, (set, tuple, list)):
+            self.outputs = {k: outputs.count(k) for k in set(outputs)}
+        elif isinstance(outputs, str):
+            self.outputs = {outputs: 1}
+        elif isinstance(outputs, dict):
+            self.outputs = outputs
+        else:
+            raise ValueError(f"outputs expected set, tuple, list or dict, but got {type(outputs)}")
 
         for i in [setup_time, run_time, shutdown_time, change_over_time, cost]:
             if not isinstance(i, (int, float)):
@@ -131,30 +217,36 @@ class Process(object):
         self.change_over_time = change_over_time
         self.cost = cost
 
+    def __eq__(self, other):
+        """ return True if process.output.keys() == task.requires.keys() 
+        Note:
+        As one 'production' could supply multiple tasks, the challenge is to
+        match tasks with productions, but should probably be performed in 
+        another function.
+        """
+        if isinstance(other, Task):
+            return self.outputs.keys() == other.requires.keys()
+
+        raise NotImplementedError  # ... if we haven't returned above ...
+
 
 class ResourceDemandNetwork(object):
     """ Resource-Demand Network
     Resources (machines, people, inventory) are connected in a network, where
     the edges permit transformation of tasks.
 
-    All demands are generated at the sink.
-    All resources are generated at the source.
+    Tasks must be given directly to Resources
     """
     def __init__(self):
         self.network = Graph()
-        self.tasks = []
-        self._schedule = {}  # task id, node id, task,
-        self.sinks = set()
-        self.sources = set()
-        self.resources = set()
-
-    def add_task(self, task):
-        """ Creates task at the sink"""
-        self.tasks[task.id] = task
+        self.mq = []
 
     def add_resource(self, resource):
         """ adds resource to RDN. """
         assert isinstance(resource, Resource)
+        if resource.id in self.network.nodes():
+            raise ValueError(f"Duplicate entry: {resource} is already registered.")
+
         self.network.add_node(node_id=resource.id, obj=resource)
         resource.rdn = self
 
@@ -167,12 +259,19 @@ class ResourceDemandNetwork(object):
         self.network.add_edge(client.id, resource.id, value=1)  # hop size = 1.
 
     def schedule(self):
-        self.sinks = {self.network.nodes(out_degree=0)}
-        for task in self.tasks:
-            # auction!  # TODO:FIXME!
-            # any resource that can deliver the task, should bid.
-            pass
-        self.sources = {self.network.nodes(in_degree=0)}
-        while any(not task.complete for task in self.tasks):
-            _ = [r.schedule() for r in self.resources]
+        # 1. discover new tasks:
+        resources = self.network.nodes()
+        if not resources:
+            raise SchedulingError("No resources to schedule.")
+
+        for r in resources:
+            if r.mq:
+                self.mq.append(r.id)
+
+        # 2. initiate the scheduling.
+        while self.mq:
+            r_id = self.mq.pop(0)  # resource id.
+            resource = self.network.node(node_id=r_id)
+            assert isinstance(resource, Resource)
+            resource.schedule()
 
--- tests/test_scheduling_problem.py
+++ tests/test_scheduling_problem.py
@@ -2,33 +2,133 @@ from graph.scheduling_problem import *
 
 
 def test_01():
-    """ Test from the doctorate thesis"""
+    """ Test the basics. """
     rdn = ResourceDemandNetwork()
-    rdn.add_task('A')
     try:
         rdn.schedule()
         assert False, "No resources loaded yet."
     except SchedulingError:
         assert True
+
     m1 = Resource()
     process1 = Process(inputs=['a'], outputs=['A'])
     m1.add_process(process1)
 
-    try:
-        rdn.schedule()
-        assert False
-    except SchedulingError:
-        pass
-
     m2 = Resource()
-    process2 = Process(outputs=['a'])
+    process2 = Process(outputs=['a'])  # no inputs required.
     m2.add_process(process2)
-    rdn.edge(m1, m2)
-    # The network is now ready to supply n 'A's using rdn.add_task('A')
-    # Let's review one schedule...
+
+    rdn.add_edge(m1, m2)
+
+    # The network is now ready to supply n 'A's when scheduled.
+    m1.add_task(Task('a'))
+
     rdn.schedule()
+    # Let's review one schedule...
+    assert m2.tasks[0].name == 'a'
+    assert m1.tasks[0].name == 'A'
 
 
 def test_02():
     """ Test from the doctorate thesis with strict sequencing """
+
+    # 1. Set the problem up:
+    rdn = ResourceDemandNetwork()
+
+    m1 = Resource()
+    for o, i, t in zip('ABCDEFG', 'abcdefg', [14, 7, 3, 10, 5, 6, 6]):
+        process = Process(inputs=[i], outputs=[o], run_time=t)
+        m1.add_process(process)
+
+    m2 = Resource()
+    for o, t in zip('abcdefg', [14, 7, 3, 10, 5, 6, 6]):
+        process = Process(outputs=[o], run_time=t)
+        m2.add_process(process)
+    rdn.add_edge(m1, m2)
+
+    for task in 'ABCDEFG':
+        m1.add_task(Task(task))
+
+    # 2. solve the problem
+    rdn.schedule()
+
+    # 3. evaluate whether the output is correct.
+    m1_expected = list('AEBDGFC')
+    m1_jobs = [t.name for t in m1.tasks]
+    assert m1_jobs == m1_expected
+
+    m2_expected = list('AEBDGFC')
+    m2_jobs = [t.name for t in m2.tasks]
+    assert m2_jobs == m2_expected
+
+    first_task = m1.tasks[0]
+    assert isinstance(first_task, Task)
+    assert first_task.scheduled_start == 2
+
+    last_task = m1.tasks[-1]
+    assert isinstance(last_task, Task)
+    assert last_task.scheduled_finish == 53
+
+
+def test_03():
+    """ test for two independent demands for a single bottleneck """
+    rdn = ResourceDemandNetwork()
+    d1, d2, r = Resource(), Resource(), Resource()
+
+    # p1 runtime < p2 runtime, so doing p1 before p2 will minimise total lateness.
+    p1 = Process(inputs=[], outputs=['A'], run_time=1)
+    p2 = Process(inputs=[], outputs=['B'], run_time=2)
+    r.add_process(p1)
+    r.add_process(p2)
+
+    rdn.add_edge(d1, r)
+    rdn.add_edge(d2, r)
+
+    d1.add_task('A')
+    d2.add_task('B')
+
+    rdn.schedule()
+
+    # Check that we've minimised total lateness.
+    assert r.tasks[0].name == 'A'
+    assert r.tasks[1].name == 'B'
+
+
+def test_04():
+    """ test for two independent demands for a single bottleneck """
+    rdn = ResourceDemandNetwork()
+
+    d1, d2, r = Resource(), Resource(), Resource()
+
+    # same run times == no deterministic solution.
+    p1 = Process(inputs=[], outputs=['A'], run_time=1)
+    p2 = Process(inputs=[], outputs=['B'], run_time=1)
+    r.add_process(p1)
+    r.add_process(p2)
+
+    rdn.add_edge(d1, r)
+    rdn.add_edge(d2, r)
+
+    d1.add_task('A')
+    d2.add_task('B')
+
+    rdn.schedule()
+
+    # Check that we've ordered tasks by id as the task have same runtime.
+    assert r.tasks[0].name == 'A'
+    assert r.tasks[1].name == 'B'
+
+
+def test_dynamic_01():
+    """ tests a network where tasks are added and removed between optimisation runs. """
     pass
+    # 1. setup rdn
+    # 2. add a few tasks
+    # 3. schedule
+    # 4. update a fictitious clock, as if some of the tasks where done.
+    # 5. remove the tasks that would be done if the clock would progress.
+    # (update the tasks with new start-times?)
+    # 6. assert that the schedule doesn't change
+    # 7. repeat from step 2.
+
+
