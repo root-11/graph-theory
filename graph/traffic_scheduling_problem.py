8eeef59c0f09d190291149b06e20555618c2ec1e
root-11 <bjorn.h.madsen@gmail.com>
2020-09-15 15:53:42 +0100

    Created separate module for the traffic_scheduling_problem.py

---
+++ graph/traffic_scheduling_problem.py
@@ -0,0 +1,360 @@
+from time import process_time
+from graph import Graph
+
+
+__description__ = """
+We've decided to refer to the optimisation problem of finding
+the fewest number of moves that resolve a traffic jam as
+a traffic scheduling problem.
+"""
+
+
+# ----------------- #
+# The main solver   #
+# ----------------- #
+def jam_solver(graph, loads, timeout=None):
+    """ an ensemble solver for the routing problem."""
+    check_user_input(graph, loads)
+    if timeout:
+        assert isinstance(timeout, (int, float))
+
+    moves = None
+    for method in methods:
+        try:
+            moves = method(graph, loads, timeout)
+        except TimeoutError:
+            pass
+        if moves:
+            return moves
+    return moves
+
+
+class Timer(object):
+    def __init__(self, timeout=None):
+        if timeout is None:
+            timeout = float('inf')
+        assert isinstance(timeout, (float, int))
+        self.limit = timeout
+        self.start = process_time()
+
+    def timeout_check(self):
+        if process_time() - self.start > (self.limit / 1000):
+            raise TimeoutError(f"No solution found in {TIMEOUT}ms")
+
+
+# helpers
+def check_user_input(graph, loads):
+    """ checks the user inputs to be valid. """
+    assert isinstance(graph, Graph)
+    assert isinstance(loads, dict)
+    for v in loads.values():
+        assert isinstance(v, list)
+        assert all(i in graph.nodes() for i in v)
+
+    for load_id, path in loads.items():
+        if len(path) > 1:
+            if not graph.has_path(path):
+                _, path = graph.shortest_path(path[0], path[-1])
+                loads[load_id] = path
+
+
+def path_to_moves(path):
+    """ translate best path into motion sequence.
+    :param path: list of tuples with [(load id, location), ... ]
+    """
+    moves = []
+    s1 = path[0]
+    for s2 in path[1:]:
+        for p1, p2 in zip(s1, s2):
+            if p1 != p2:
+                moves.append({p1[0]: (p1[1], p2[1])})  # {load id: (location1, location2)}
+        s1 = s2
+    return moves
+
+
+def merge(paths):
+    """
+    :param paths: [list of nodes]
+    :return: path as list of nodes
+    """
+    g = Graph()
+    for path in paths:
+        a = path[0]
+        for b in path[1:]:
+            v = g.edge(a, b)
+            if v is None:
+                g.add_edge(a, b, 1)
+            else:
+                g.add_edge(a, b, v + 1)
+            a = b
+    start, end = g.nodes(in_degree=0), g.nodes(out_degree=0)
+    d, p = g.shortest_path(start[0], end[0])
+    return p
+
+
+def test_merge():
+    common_path = merge([[1, 2, 3, 4, 5, 9, 10, 11, 12],
+                         [2, 3, 4, 5, 9, 10, 11, 12, 13],
+                         [3, 4, 5, 9, 10, 11, 12, 13, 14]])
+    assert common_path == [1, 2, 3, 4, 5, 9, 10, 11, 12, 13, 14], common_path
+
+
+def bfs_resolve(graph, loads, timeout=None):
+    """
+    calculates the solution to the transshipment problem by
+    constructing the solution space as a finite state machine
+    and then finding the shortest path through the fsm from the
+    initial state to the desired state.
+    """
+    initial_state = tuple(((load_id, route[0]) for load_id, route in loads.items()))
+    final_state = tuple(((load_id, route[-1]) for load_id, route in loads.items()))
+    movements = Graph()
+
+    states = [initial_state]
+
+    timer = Timer(timeout=timeout)
+    solved = False
+
+    while not solved:
+        timer.timeout_check()
+
+        state = states.pop(0)
+        occupied = {i[1] for i in state}
+        for load_id, location in state:
+            if solved: break
+            options = (e for s, e, d in graph.edges(from_node=location) if e not in occupied)
+            for option in options:
+                new_state = tuple((lid, loc) if lid != load_id else (load_id, option) for lid, loc in state)
+
+                if new_state in movements:  # abandon branch
+                    continue
+                else:
+                    movements.add_edge(state, new_state, 1)
+                    states.append(new_state)
+
+                if final_state == new_state:
+                    solved = True
+                    break
+        if not states:
+            raise Exception("No solution found")
+
+    steps, best_path = movements.shortest_path(initial_state, final_state)
+    moves = path_to_moves(best_path)
+    return moves
+
+
+def bi_directional_progressive_bfs(graph, loads, timeout=None):
+    """ Bi-directional search which searches to the end of open options for each load. """
+    initial_state = tuple(((load_id, route[0]) for load_id, route in loads.items()))
+    final_state = tuple(((load_id, route[-1]) for load_id, route in loads.items()))
+
+    movements = Graph()
+    forward_queue = [initial_state]
+    forward_states = {initial_state}
+    reverse_queue = [final_state]
+    reverse_states = {final_state}
+
+    timer = Timer(timeout=timeout)
+    solved = False
+
+    while not solved:
+        timer.timeout_check()
+
+        # forward
+        state = forward_queue.pop(0)
+        occupied = {i[1] for i in state}
+        for load_id, location in state:
+            if solved:
+                break
+            options = {e: state for s, e, d in graph.edges(from_node=location) if e not in occupied}
+            if not options:
+                continue
+
+            been = {i for i in occupied}
+            while options:
+                option = list(options.keys())[0]
+                old_state = options.pop(option)  # e from s,e,d
+
+                new_state = tuple((lid, loc) if lid != load_id else (load_id, option) for lid, loc in old_state)
+                if new_state not in movements:
+                    forward_queue.append(new_state)
+
+                movements.add_edge(old_state, new_state, 1)
+                forward_states.add(new_state)
+
+                been.add(option)
+                options.update({e: new_state for s, e, d in graph.edges(from_node=option) if e not in been})
+
+                if new_state in reverse_states:
+                    solved = True
+                    break
+
+        # backwards
+        state = reverse_queue.pop(0)
+        occupied = {i[1] for i in state}
+        for load_id, location in state:
+            if solved:
+                break
+
+            options = {s: state for s, e, d in graph.edges(to_node=location) if s not in occupied}
+            if not options:
+                continue
+
+            been = {i for i in occupied}
+            while options:
+                option = list(options.keys())[0]  # s from s,e,d
+                old_state = options.pop(option)
+
+                new_state = tuple((lid, loc) if lid != load_id else (load_id, option) for lid, loc in old_state)
+
+                if new_state not in movements:  # add to queue
+                    reverse_queue.append(new_state)
+
+                movements.add_edge(new_state, old_state, 1)
+                reverse_states.add(new_state)
+
+                been.add(option)
+                options.update({s: new_state for s, e, d in graph.edges(to_node=option) if s not in been})
+
+                if new_state in forward_states:
+                    solved = True
+                    break
+
+    steps, best_path = movements.shortest_path(initial_state, final_state)
+    moves = path_to_moves(best_path)
+    return moves
+
+
+def bi_directional_bfs(graph, loads, timeout=None):
+    """ calculates the solution to the transshipment problem using BFS
+    from both initial and final state """
+    initial_state = tuple(((load_id, route[0]) for load_id, route in loads.items()))
+    final_state = tuple(((load_id, route[-1]) for load_id, route in loads.items()))
+
+    movements = Graph()
+    forward_queue = [initial_state]
+    forward_states = {initial_state}
+    reverse_queue = [final_state]
+    reverse_states = {final_state}
+
+    timer = Timer(timeout=timeout)
+    solved = False
+
+    while not solved:
+        timer.timeout_check()
+
+        # forward
+        state = forward_queue.pop(0)
+        occupied = {i[1] for i in state}
+        for load_id, location in state:
+            if solved:
+                break
+            options = [e for s, e, d in graph.edges(from_node=location) if e not in occupied]
+            for option in options:
+                new_state = tuple((lid, loc) if lid != load_id else (load_id, option) for lid, loc in state)
+                if new_state not in movements:
+                    forward_queue.append(new_state)
+
+                movements.add_edge(state, new_state, 1)
+                forward_states.add(new_state)
+
+                if new_state in reverse_states:
+                    solved = True
+                    break
+
+        # backwards
+        state = reverse_queue.pop(0)
+        occupied = {i[1] for i in state}
+        for load_id, location in state:
+            if solved:
+                break
+            options = [s for s, e, d in graph.edges(to_node=location) if s not in occupied]
+            for option in options:
+                new_state = tuple((lid, loc) if lid != load_id else (load_id, option) for lid, loc in state)
+
+                if new_state not in movements:  # add to queue
+                    reverse_queue.append(new_state)
+
+                movements.add_edge(new_state, state, 1)
+                reverse_states.add(new_state)
+
+                if new_state in forward_states:
+                    solved = True
+                    break
+
+    steps, best_path = movements.shortest_path(initial_state, final_state)
+    moves = path_to_moves(best_path)
+    return moves
+
+
+def dfs_resolve(graph, loads, timeout=None):  # <-- this is useless.
+    """
+    calculates the solution to the transshipment problem by
+    search along a line of movements and backtracking when it
+    no longer leads anywhere (DFS).
+    """
+    initial_state = tuple(((load_id, route[0]) for load_id, route in loads.items()))
+    final_state = tuple(((load_id, route[-1]) for load_id, route in loads.items()))
+
+    state = initial_state
+    states = [initial_state]  # q
+    path = []
+    movements = Graph()
+    visited = set()
+
+    timer = Timer(timeout=timeout)
+
+    while states:
+        timer.timeout_check()
+
+        state = states.pop(0)  # n1
+        visited.add(state)  # visited
+        path.append(state)  # path
+        if state == final_state:
+            states.clear()  # return path  # exit if final state is found.
+            break
+
+        for new_state in new_states(graph, movements, state):  # for n2 in g.from(n1)
+            if new_state in visited:  # if n2 in visited
+                continue
+            states.append(new_state)  # q.append(n2)
+            break
+        else:
+            path.remove(state)  # path.remove(n1)
+            while not states and path:  # while not q and path
+                for new_state in new_states(graph, movements, state=path[-1]):  # for n2 in g.from_node(path[-1]):..
+                    if new_state in visited:  # if n2 in visited
+                        continue
+                    states.append(new_state)
+                    break
+                else:
+                    path = path[:-1]
+    if state != final_state:
+        return None  # <-- exit if not path was found.
+
+    steps, best_path = movements.shortest_path(initial_state, final_state)
+    moves = path_to_moves(best_path)
+    return moves
+
+
+def new_states(graph, movements, state):
+        occupied = {i[1] for i in state}
+        for load_id, location in state:
+            options = (e for s, e, d in graph.edges(from_node=location) if e not in occupied)
+            for option in options:
+                new_state = tuple((lid, loc) if lid != load_id else (load_id, option) for lid, loc in state)
+                if new_state in movements:
+                    continue
+                movements.add_edge(state, new_state, 1)
+                yield new_state
+
+
+# collection of solution methods for the routing problem.
+# insert, delete, append or substitute with your own methods as required.
+methods = [
+    bi_directional_progressive_bfs,  # <-- the fastest, but not always the best method.
+    bi_directional_bfs,  # <-- best method so far.
+    bfs_resolve,  # very slow, but will eventually find the best solution.
+    # dfs_resolve,  <--- this simply doesn't work.
+]
+
---
+++ tests/test_traffic_scheduling_problem.py
@@ -0,0 +1,337 @@
+from time import process_time
+
+from graph import Graph
+from tests.test_graph import graph5x5
+
+from graph.traffic_scheduling_problem import jam_solver, bi_directional_bfs, bi_directional_progressive_bfs
+from graph.traffic_scheduling_problem import check_user_input
+
+
+def test_simple_reroute():
+    """ to loads on a collision path. """
+    g = Graph()
+    for s, e in [(1, 2), (2, 3)]:
+        g.add_edge(s, e, 1, bidirectional=True)
+    for s, e in [(1, 4), (4, 3)]:
+        g.add_edge(s, e, 1, bidirectional=False)
+
+    loads = {1: [1, 2, 3], 2: [3, 2, 1]}
+
+    sequence = jam_solver(g, loads)
+    assert sequence == [{1: (1, 4)},
+                        {2: (3, 2)},
+                        {2: (2, 1)},
+                        {1: (4, 3)}]
+
+
+def test_simple_reroute_2():
+    """ to loads on a collision path. """
+    g = Graph()
+    for s, e in [(1, 2), (2, 3), (3, 4)]:
+        g.add_edge(s, e, 1, bidirectional=True)
+    for s, e in [(1, 5), (5, 6), (6, 4)]:
+        g.add_edge(s, e, 1, bidirectional=False)
+
+    loads = {1: [1, 2, 3, 4], 2: [4, 3, 2, 1]}
+
+    sequence = jam_solver(g, loads)
+    assert sequence == [{1: (1, 5)},
+                        {2: (4, 3)},
+                        {2: (3, 2)},
+                        {2: (2, 1)},
+                        {1: (5, 6)},
+                        {1: (6, 4)}]
+
+
+def test_simple_reroute_3():
+    """ Loop with 6 nodes:
+    1 <--> 2 <--> 3 <--> 4 <-- 5 <--> 6 <--> (1)
+    """
+    g = Graph()
+    edges = [(1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 1)]
+    for s, e in edges:
+        g.add_edge(s, e, 1, bidirectional=True)
+    g.del_edge(4, 5)
+
+    loads = {1: [1, 2, 3], 2: [3, 2, 1]}
+
+    sequence = jam_solver(g, loads)
+
+    assert sequence == [{1: (1, 6)}, {2: (3, 2)}, {2: (2, 1)}, {1: (6, 5)}, {1: (5, 4)}, {1: (4, 3)}], sequence
+    # assert sequence == [{1: (1, 6)}, {1: (6, 5)}, {2: (3, 2)}, {2: (2, 1)}, {1: (5, 4)}, {1: (4, 3)}], sequence
+
+
+def test_simple_reroute_4():
+    """
+        1
+       / \
+      6---2
+     / \ / \
+    5 - 4 - 3
+    """
+    g = Graph()
+    edges = [(1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 1), (2, 6), (2, 4), (6, 2)]
+    for s, e in edges:
+        g.add_edge(s, e, 1, bidirectional=True)
+    g.del_edge(4, 5)
+
+    loads = {1: [1, 2, 3, 4],
+             3: [3, 2, 1],
+             6: [6, 2]}
+
+    sequence = jam_solver(g, loads)
+    assert sequence == [{1: (1, 2)}, {1: (2, 4)}, {3: (3, 2)}, {3: (2, 1)}, {6: (6, 2)}]
+
+    g.del_edge(2, 4)
+
+    sequence = jam_solver(g, loads)
+    assert sequence == [{1: (1, 2)}, {3: (3, 4)}, {1: (2, 3)}, {3: (4, 2)}, {3: (2, 1)}, {6: (6, 2)}, {1: (3, 4)}]
+
+
+def test_clockwise_rotation():
+    """ A simple loop of 4 locations, where 3 loads need to move
+    clockwise. """
+    g = Graph()
+    edges = [(1, 2), (2, 3), (3, 4), (4, 1), ]
+    for s, e in edges:
+        g.add_edge(s, e, 1, bidirectional=True)
+
+    loads = {1: [1, 2], 2: [2, 3], 3: [3, 4]}  # position 4 is empty.
+
+    sequence = jam_solver(g, loads)
+
+    assert sequence == [{3: (3, 4)},  # first move.
+                        {2: (2, 3)},  # second move.
+                        {1: (1, 2)}], sequence  # last move.
+
+
+def test_small_gridlock():
+    """ a grid lock is given, solver solves it."""
+    g = Graph()
+    edges = [
+        (1, 2), (1, 4), (2, 3), (2, 5), (3, 6), (4, 5), (5, 6), (4, 7), (5, 8), (6, 9), (7, 8), (8, 9)
+    ]
+    for s, e in edges:
+        g.add_edge(s, e, 1, bidirectional=True)
+
+    loads = {'a': [2, 1], 'b': [5, 2], 'c': [4, 3], 'd': [8], 'e': [1, 9]}
+    check_user_input(g, loads)
+
+    start = process_time()
+    sequence = bi_directional_progressive_bfs(g, loads)
+    end = process_time()
+    print("duration:", end - start, "bi_directional_progressive_bfs")
+
+    assert sequence == [{'a': (2, 3)},
+                        {'a': (3, 6)},
+                        {'a': (6, 9)},
+                        {'b': (5, 2)},
+                        {'c': (4, 5)},
+                        {'c': (5, 6)},
+                        {'c': (6, 3)},
+                        {'a': (9, 6)},
+                        {'a': (6, 5)},
+                        {'a': (5, 4)},
+                        {'a': (4, 7)},
+                        {'e': (1, 4)},
+                        {'e': (4, 5)},
+                        {'e': (5, 6)},
+                        {'e': (6, 9)},
+                        {'a': (7, 4)},
+                        {'a': (4, 1)}]
+    start = process_time()
+    sequence = bi_directional_bfs(g, loads)
+    end = process_time()
+    print("duration:", end - start, "bi_directional_bfs", flush=True)
+
+    assert sequence == [{'b': (5, 6)},
+                        {'b': (6, 3)},
+                        {'c': (4, 5)},
+                        {'c': (5, 6)},
+                        {'e': (1, 4)},
+                        {'a': (2, 1)},
+                        {'e': (4, 5)},
+                        {'b': (3, 2)},
+                        {'c': (6, 3)},
+                        {'e': (5, 6)},
+                        {'e': (6, 9)}], sequence
+
+
+def test_snake_gridlock():
+    """
+    A bad route was given to train abcd, and now the train has gridlocked itself.
+
+                    9 - 10 - 11 - 12
+                    |
+    1 - 2 - 3 - 4 - 5d-> 6c
+                    ^    |
+                    |    v
+                    8a - 7b
+
+    :return:
+    """
+    g = Graph()
+    edges = [(a, b) for a, b in zip(range(1, 12), range(2, 13)) if (a, b) != (8, 9)]
+    for s, e in edges:
+        g.add_edge(s, e, 1, bidirectional=True)
+    g.add_edge(8, 5, 1, bidirectional=True)
+    g.add_edge(5, 9, 1, bidirectional=True)
+
+    loads = {'a': [8, 5, 9, 10, 11, 12], 'b': [7, 8, 5, 9, 10, 11], 'c': [6, 7, 8, 5, 9, 10], 'd': [5, 6, 7, 8, 5, 9]}
+    sequence = jam_solver(g, loads)
+
+    assert sequence == [{'d': (5, 4)},  # d goes one step back.
+                        {'a': (8, 5)},  # a moves forward to it's destination.
+                        {'a': (5, 9)},
+                        {'a': (9, 10)},
+                        {'a': (10, 11)},
+                        {'a': (11, 12)},
+                        {'b': (7, 8)},  # b moves forward to it's destination.
+                        {'b': (8, 5)},
+                        {'b': (5, 9)},
+                        {'b': (9, 10)},
+                        {'b': (10, 11)},
+                        {'c': (6, 5)},  # c moves forward.
+                        {'c': (5, 9)},
+                        {'c': (9, 10)},
+                        {'d': (4, 5)},  # d does a shortcut.
+                        {'d': (5, 9)}]
+
+
+def test_5x5_graph():
+    g = graph5x5()
+    loads = {'a': [6], 'b': [11, 1], 'c': [16, 2], 'd': [17, 4], 'e': [19, 5], 'f': [20, 3]}
+
+    sequence = jam_solver(g, loads)
+
+    assert sequence == [{'a': (6, 7)},
+                        {'b': (11, 6)},
+                        {'b': (6, 1)},
+                        {'a': (7, 6)},
+                        {'c': (16, 11)},
+                        {'d': (17, 18)},
+                        {'d': (18, 13)},
+                        {'d': (13, 14)},
+                        {'d': (14, 9)},
+                        {'d': (9, 4)},
+                        {'f': (20, 15)},
+                        {'f': (15, 14)},
+                        {'f': (14, 13)},
+                        {'f': (13, 8)},
+                        {'f': (8, 3)},
+                        {'e': (19, 20)},
+                        {'e': (20, 15)},
+                        {'e': (15, 10)},
+                        {'e': (10, 5)},
+                        {'c': (11, 12)},
+                        {'c': (12, 7)},
+                        {'c': (7, 2)}], sequence
+
+
+def test_2_trains():
+    """
+    two trains of loads are approaching each other.
+    train 123 going from 1 to 14
+    train 4567 going from 14 to 1.
+
+    At intersection  4 train 123 can be broken apart and
+    buffered, so that train 4567 can pass.
+
+    The reverse (buffering train 4567) is not possible.
+    """
+    g = Graph()
+    edges = [
+        (1, 2),
+        (2, 3),
+        (3, 4),
+        (4, 5), (4, 6), (4, 7), (4, 8),
+        (5, 9), (6, 9), (7, 9), (8, 9),
+        (9, 10),
+        (10, 11),
+        (11, 12),
+        (12, 13),
+        (13, 14),
+    ]
+    for s, e in edges:
+        g.add_edge(s, e, 1, bidirectional=True)
+
+    loads = {
+        41: [1, 2, 3, 4, 5, 9, 10, 11, 12],
+        42: [2, 3, 4, 5, 9, 10, 11, 12, 13],
+        43: [3, 4, 5, 9, 10, 11, 12, 13, 14],
+        44: [11, 10, 9, 5, 4, 3, 2, 1],
+        45: [12, 11, 10, 9, 5, 4, 3, 2],
+        46: [13, 12, 11, 10, 9, 5, 4, 3],
+        47: [14, 13, 12, 11, 10, 9, 5, 4],
+    }
+
+    sequence = jam_solver(g, loads)
+
+    assert sequence == [{43: (3, 4)}, {43: (4, 6)}, {42: (2, 3)}, {42: (3, 4)}, {42: (4, 7)}, {41: (1, 2)},
+                        {41: (2, 3)}, {41: (3, 4)}, {41: (4, 5)}, {44: (11, 10)}, {44: (10, 9)}, {44: (9, 8)},
+                        {44: (8, 4)}, {44: (4, 3)}, {44: (3, 2)}, {44: (2, 1)}, {45: (12, 11)}, {45: (11, 10)},
+                        {45: (10, 9)}, {45: (9, 8)}, {45: (8, 4)}, {45: (4, 3)}, {45: (3, 2)}, {46: (13, 12)},
+                        {46: (12, 11)}, {46: (11, 10)}, {46: (10, 9)}, {46: (9, 8)}, {46: (8, 4)}, {46: (4, 3)},
+                        {47: (14, 13)}, {47: (13, 12)}, {47: (12, 11)}, {47: (11, 10)}, {47: (10, 9)}, {47: (9, 8)},
+                        {47: (8, 4)}, {43: (6, 9)}, {43: (9, 10)}, {43: (10, 11)}, {43: (11, 12)}, {43: (12, 13)},
+                        {43: (13, 14)}, {42: (7, 9)}, {42: (9, 10)}, {42: (10, 11)}, {42: (11, 12)}, {42: (12, 13)},
+                        {41: (5, 9)}, {41: (9, 10)}, {41: (10, 11)}, {41: (11, 12)}], sequence
+
+
+def test_3_trains():
+    """
+    Two trains (abc & d) are going east. One train is going west (efgh).
+
+    a-b-c--0-0-0--d--0--e-f-g-h
+         \---0---/ \-0-/
+
+    1-2-3--4-5-6--7--8---9-10-11-12
+         \---13--/ \-14-/
+
+    The solution is given by side stepping abc & d and letting efgh pass.
+    """
+    g = Graph()
+    edges = [
+        (3, 13), (13, 7), (7, 14), (14, 9)
+    ]
+    for a, b in zip(range(1, 12), range(2, 13)):
+        edges.append((a, b))
+    for s, e in edges:
+        g.add_edge(s, e, 1, bidirectional=True)
+
+    loads = {
+        'a': [1, 10], 'b': [2, 11], 'c': [3, 12], 'd': [8, 9],  # east bound
+        'e': [9, 1], 'f': [10, 2], 'g': [11, 3], 'h': [12, 4]  # west bound
+    }
+
+    sequence = jam_solver(g, loads)
+    assert sequence is not None
+
+
+def test_4_trains():
+    """ move through a bottleneck
+    123 -0---0---456
+         |   |
+    789 -0---0---901
+
+    456-123
+    901-789
+
+    901-123
+    456-789
+
+    789-123
+    901-456
+    """
+    assert True  # TODO
+
+
+def test_random():
+    """
+    Generate random network
+    Add loads at random
+    Try to solve. Add to test suite if failed.
+
+    Remember: The test suite can run forever. But the solver must be quick.
+    """
+    assert True  # TODO
