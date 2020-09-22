4b98dfd6952bde6fbf1c0db86c9504133ec35983
root-11 <bjorn.h.madsen@gmail.com>
2020-09-22 12:01:13 +0100

    Removed idle code.

--- graph/traffic_scheduling_problem.py
+++ graph/traffic_scheduling_problem.py
@@ -86,33 +86,6 @@ def path_to_moves(path):
     return moves
 
 
-def merge(paths):
-    """
-    :param paths: [list of nodes]
-    :return: path as list of nodes
-    """
-    g = Graph()
-    for path in paths:
-        a = path[0]
-        for b in path[1:]:
-            v = g.edge(a, b)
-            if v is None:
-                g.add_edge(a, b, 1)
-            else:
-                g.add_edge(a, b, v + 1)
-            a = b
-    start, end = g.nodes(in_degree=0), g.nodes(out_degree=0)
-    d, p = g.shortest_path(start[0], end[0])
-    return p
-
-
-def test_merge():
-    common_path = merge([[1, 2, 3, 4, 5, 9, 10, 11, 12],
-                         [2, 3, 4, 5, 9, 10, 11, 12, 13],
-                         [3, 4, 5, 9, 10, 11, 12, 13, 14]])
-    assert common_path == [1, 2, 3, 4, 5, 9, 10, 11, 12, 13, 14], common_path
-
-
 def bfs_resolve(graph, loads, timeout=None):
     """
     calculates the solution to the transshipment problem by
@@ -326,83 +299,11 @@ def bi_directional_bfs(graph, loads, timeout=None):
     return moves
 
 
-def dfs_resolve(graph, loads, timeout=None):  # <-- this is useless.
-    """
-    calculates the solution to the transshipment problem by
-    search along a line of movements and backtracking when it
-    no longer leads anywhere (DFS).
-
-    :param graph network available for routing.
-    :param loads: dictionary with load id and preferred route. Example:
-
-        loads = {1: [1, 2, 3], 2: [3, 2, 1]}
-
-    :param timeout: None, float or int timeout in milliseconds.
-
-    """
-    initial_state = tuple(((load_id, route[0]) for load_id, route in loads.items()))
-    final_state = tuple(((load_id, route[-1]) for load_id, route in loads.items()))
-
-    state = initial_state
-    states = [initial_state]  # q
-    path = []
-    movements = Graph()
-    visited = set()
-
-    timer = Timer(timeout=timeout)
-
-    while states:
-        timer.timeout_check()
-
-        state = states.pop(0)  # n1
-        visited.add(state)  # visited
-        path.append(state)  # path
-        if state == final_state:
-            states.clear()  # return path  # exit if final state is found.
-            break
-
-        for new_state in new_states(graph, movements, state):  # for n2 in g.from(n1)
-            if new_state in visited:  # if n2 in visited
-                continue
-            states.append(new_state)  # q.append(n2)
-            break
-        else:
-            path.remove(state)  # path.remove(n1)
-            while not states and path:  # while not q and path
-                for new_state in new_states(graph, movements, state=path[-1]):  # for n2 in g.from_node(path[-1]):..
-                    if new_state in visited:  # if n2 in visited
-                        continue
-                    states.append(new_state)
-                    break
-                else:
-                    path = path[:-1]
-    if state != final_state:
-        return None  # <-- exit if not path was found.
-
-    steps, best_path = movements.shortest_path(initial_state, final_state)
-    moves = path_to_moves(best_path)
-    return moves
-
-
-def new_states(graph, movements, state):
-    """ generator for new states for DFS. """
-    occupied = {i[1] for i in state}
-    for load_id, location in state:
-        options = (e for s, e, d in graph.edges(from_node=location) if e not in occupied)
-        for option in options:
-            new_state = tuple((lid, loc) if lid != load_id else (load_id, option) for lid, loc in state)
-            if new_state in movements:
-                continue
-            movements.add_edge(state, new_state, 1)
-            yield new_state
-
-
 # collection of solution methods for the routing problem.
 # insert, delete, append or substitute with your own methods as required.
 methods = [
     bi_directional_progressive_bfs,  # <-- the fastest, but not always the best method.
     bi_directional_bfs,  # <-- best method so far.
     bfs_resolve,  # very slow, but will eventually find the best solution.
-    # dfs_resolve,  <--- this simply doesn't work.
 ]
 
