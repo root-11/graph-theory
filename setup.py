dd3ec1f9d74191bb54b54dc1802c454d00f39db9
root-11 <bjorn.h.madsen@gmail.com>
2021-10-18 09:19:35 +0100

    Added missing keywords to the setup.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 """
 graph-theory
 """
-build_tag = "9272a566429d8f27da3ad7daf364ed0dd503c8cfe3dbbf611bf359180d5c67af"
+build_tag = "cb75e98d6b92893eaab533e0b1383ec4ca5a0d2dec41fc7143659e5240fda3a4"
 from setuptools import setup
 from pathlib import Path
 
@@ -21,7 +21,8 @@ keywords = list({
     'assignment problem', 'flow-problem', 'hash', 'graph-hash', 'random graph', 'search', 'cycle', 'path',
     'component', 'components', 'adjacency', 'matrix', 'all pairs shortest path', 'finite state machine', 'fsm',
     'traffic-jam', 'traffic-jam-solver', 'solver', 'hill-climbing', 'simple', 'simple-path', 'critical', 'path',
-    'method', 'critical-path', 'minimize', 'minimise', 'optimize', 'optimise'
+    'method', 'critical-path', 'minimize', 'minimise', 'optimize', 'optimise', 'merkle', 'tree', 'merkle-tree',
+    'hash-tree'
 })
 
 keywords.sort(key=lambda x: x.lower())
@@ -29,7 +30,7 @@ keywords.sort(key=lambda x: x.lower())
 
 setup(
     name="graph-theory",
-    version="2021.4.1.45216",
+    version="2021.8.22.41289",
     url="https://github.com/root-11/graph-theory",
     license="MIT",
     author="Bjorn Madsen",
