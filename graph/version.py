e34267e897050125d1136cbfc70dbd4c527e183a
Bjorn Madsen <dr.bjorn.madsen@gmail.com>
2022-11-23 14:50:56 +0000

    Updates to setup and workflow as github actions now will package to pypi.

---
+++ graph/version.py
@@ -0,0 +1,3 @@
+major, minor, patch = 2022, 3, "dev1"
+__version_info__ = (major, minor, patch)
+__version__ = ".".join(str(i) for i in __version_info__)
--- setup.py
+++ setup.py
@@ -1,58 +1,130 @@
 """
 graph-theory
 """
-build_tag = "f92033ea46b04af69578081a183c0793520e3f1ab10707fd4e42df7391278894"
 from setuptools import setup
 from pathlib import Path
 
 
-folder = Path(__file__).parent
-file = "README.md"
-readme = folder / file
-assert isinstance(readme, Path)
-assert readme.exists(), readme
-with open(str(readme), encoding='utf-8') as f:
+root = Path(__file__).parent
+
+__version__ = None
+version_file = root / "tablite" / "version.py"
+exec(version_file.read_text())
+assert isinstance(__version__, str)  # noqa
+
+with open(root / "README.md", encoding="utf-8") as f:
     long_description = f.read()
 
-keywords = list({
-    'complex-networks', 'discrete mathematics', 'graph', 'Graph Theory', 'graph-algorithms', 'graph-analysis', 'analysis', 'algorithms',
-    'graph-generation', 'graph-theory', 'graph-visualization', 'graphs', 'math', 'Mathematics', 'maths', 'generation', 'generate', 'theory',
-    'minimum-spanning-trees', 'network', 'Networks', 'optimization', 'python', 'shortest-path', 'tsp', 'tsp-solver', 'minimum', 'spanning', 'tree',
-    'assignment problem', 'flow-problem', 'hash', 'graph-hash', 'random graph', 'search', 'cycle', 'path', 'flow', 'path', 'shortest',
-    'component', 'components', 'adjacency', 'matrix', 'all pairs shortest path', 'finite state machine', 'fsm', 'adjacent', 'pairs', 'finite', 'state', 'machine',
-    'traffic-jam', 'traffic-jam-solver', 'solver', 'hill-climbing', 'simple', 'simple-path', 'critical', 'path', 'congestions', 'jam', 'traffic', 'optimisation',
-    'method', 'critical-path', 'minimize', 'minimise', 'optimize', 'optimise', 'merkle', 'tree', 'merkle-tree',
-    'hash-tree'
-})
+with open(root / 'requirements.txt', 'r', encoding='utf-8') as fi:
+    requirements = [v.rstrip('\n') for v in fi.readlines()]
+
+keywords = list(
+    {
+        "complex-networks",
+        "discrete mathematics",
+        "graph",
+        "Graph Theory",
+        "graph-algorithms",
+        "graph-analysis",
+        "analysis",
+        "algorithms",
+        "graph-generation",
+        "graph-theory",
+        "graph-visualization",
+        "graphs",
+        "math",
+        "Mathematics",
+        "maths",
+        "generation",
+        "generate",
+        "theory",
+        "minimum-spanning-trees",
+        "network",
+        "Networks",
+        "optimization",
+        "python",
+        "shortest-path",
+        "tsp",
+        "tsp-solver",
+        "minimum",
+        "spanning",
+        "tree",
+        "assignment problem",
+        "flow-problem",
+        "hash",
+        "graph-hash",
+        "random graph",
+        "search",
+        "cycle",
+        "path",
+        "flow",
+        "path",
+        "shortest",
+        "component",
+        "components",
+        "adjacency",
+        "matrix",
+        "all pairs shortest path",
+        "finite state machine",
+        "fsm",
+        "adjacent",
+        "pairs",
+        "finite",
+        "state",
+        "machine",
+        "traffic-jam",
+        "traffic-jam-solver",
+        "solver",
+        "hill-climbing",
+        "simple",
+        "simple-path",
+        "critical",
+        "path",
+        "congestions",
+        "jam",
+        "traffic",
+        "optimisation",
+        "method",
+        "critical-path",
+        "minimize",
+        "minimise",
+        "optimize",
+        "optimise",
+        "merkle",
+        "tree",
+        "merkle-tree",
+        "hash-tree",
+    }
+)
 
 keywords.sort(key=lambda x: x.lower())
 
 
 setup(
     name="graph-theory",
-    version="2022.1.8.66708",
+    version=__version__,
     url="https://github.com/root-11/graph-theory",
     license="MIT",
-    author="Dr. Bjorn Madsen",
+    author="https://github.com/root-11",
     description="A graph library",
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     keywords=keywords,
     packages=["graph"],
+    python_requires=">-3.7",
     include_package_data=True,
-    data_files=[(".", ["LICENSE", "README.md"])],
+    data_files=[(".", ["LICENSE", "README.md", "requirements.txt"])],
     platforms="any",
-    install_requires=[],
+    install_requires=requirements,
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Science/Research",
         "Natural Language :: English",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
 )
-
-
